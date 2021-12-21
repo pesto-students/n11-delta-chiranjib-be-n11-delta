086ab71ad531089ba3514bc883d08dfd202b1199
Shubham Bansal <svb@Shubhams-MacBook-Air.local>
2021-12-21 20:28:09 +0530

    small in admin screen

--- routes/userList.js
+++ routes/userList.js
@@ -21,6 +21,7 @@ router.get("/bytimestamp", function (req, res, next) {
       });
     })
     .catch((error) => {
+      console.log(error);
       res.status(500).json({
         error: "Internal Server Error.",
       });
