1260205ca67052d861b79aeb143e75df47d9442f
Alisha <alisha.mahajan265@gmail.com>
2021-12-24 12:06:37 +0530

    fix(refresh-token): fix implementation in refresh token handling

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -5,19 +5,10 @@ const jwt = require("jsonwebtoken");
 
 const ErrorCodes = require("../core/constants");
 
-// GET Books List
 router.post("/", function (req, res, next) {
-  // request params
-  let token = req.query.token;
-  let refreshToken = req.query.refresh_token;
+  const refreshToken = req.body.refreshToken;
 
-  if (!!token) {
-    return res.status(ErrorCodes.BAD_REQUEST).json({
-      message: "No token provided",
-    });
-  }
-
-  if (!!refreshToken) {
+  if (!refreshToken) {
     return res.status(ErrorCodes.BAD_REQUEST).json({
       message: "No refresh token provided",
     });
